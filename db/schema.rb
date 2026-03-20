a0bde7b0d4a620433ceacac441486e1b84979762
dylanlurr <wangdylanlorrenzo88@gmail.com>
2026-03-20 12:06:37 +0700

    Update CI workflow: Setup PostgreSQL untuk automated testing

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.1].define(version: 2026_03_18_074559) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
   create_table "production_tasks", force: :cascade do |t|
     t.boolean "completed"
     t.datetime "created_at", null: false
